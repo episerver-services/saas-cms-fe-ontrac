8f7706e1fd49b5ac18d55915c08df64f7cacd98d
Owen Liversidge <owenliversidge@Owens-MacBook-Pro-2.local>
2025-07-29 17:13:45 +0100

    add storybook

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
+
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -13,27 +16,19 @@ const eslintConfig = [
   // Ignore these files
   {
     ignores: ['lib/optimizely/sdk.ts', 'app/globals.css'],
-  },
-
-  // Apply the base config
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-
-  // Global rule overrides
+  }, // Apply the base config
+  ...compat.extends('next/core-web-vitals', 'next/typescript'), // Global rule overrides
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
-  },
-
-  // ✅ Allow `require()` in PostCSS config files
+  }, // ✅ Allow `require()` in PostCSS config files
   {
     files: ['postcss.config.{js,cjs,mjs}'],
     rules: {
       '@typescript-eslint/no-require-imports': 'off',
     },
-  },
-
-  // ✅ Disable linting rules for CSS files
+  }, // ✅ Disable linting rules for CSS files
   {
     files: ['**/*.css'],
     rules: {
@@ -42,6 +37,7 @@ const eslintConfig = [
       'import/no-unresolved': 'off',
     },
   },
+  ...storybook.configs['flat/recommended'],
 ]
 
 export default eslintConfig
